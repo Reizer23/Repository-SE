26566e8fe2bea9ca86a5f2759d8273b977cbf626
Reizer23 <35178389+Reizer23@users.noreply.github.com>
2018-05-20 12:59:03 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package reizer.company.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hellow World");
+        System.out.println("Great Job Reizer");
+    }
+}
