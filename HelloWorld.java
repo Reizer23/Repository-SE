80c59aa08f63b579f184651db27cfb43fdca8dc4
Reizer23 <35178389+Reizer23@users.noreply.github.com>
2018-06-12 11:14:48 -0500

    no need it

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hellow World");
-        System.out.println("Great Job Reizer");
-	//First commit from console
-    }
-}
